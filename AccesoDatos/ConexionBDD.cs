c23250cbe376423b2bd440fd81cc35d57336cc31
naomyamador_04 <naomyabigail2009@gmail.com>
2026-04-29 18:42:37 -0400

    Clase ConexionBDD creada con el servidor a usar

---
+++ AccesoDatos/ConexionBDD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class ConexionBDD
+    {
+        private string CadenaConexion = "server=LAPTOP-PHTCMGVS\\SQLEXPRESS; database=SistemaVotacionEscolar; integrated security=true;";
+
+        public SqlConnection ObtenerConexion()
+        {
+            SqlConnection Conexion = new SqlConnection(CadenaConexion);
+            Conexion.Open();
+            return Conexion;
+        }
+    }
+}

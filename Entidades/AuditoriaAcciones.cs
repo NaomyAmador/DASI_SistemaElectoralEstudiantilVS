12f29a88c85cee1437b2cc9324c709cb4435f66b
Arianna <ariannacedenob@gmail.com>
2026-05-03 19:52:07 -0400

    Entidades terminadas completamente

---
+++ Entidades/AuditoriaAcciones.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class AuditoriaAcciones
+    {
+        public int AuditoriaID { get; set; }
+        public int UsuarioID { get; set; }
+        public string Accion {  get; set; }
+        public DateTime FechaHora { get; set; }
+        public string Detalle {  get; set; } 
+    }
+}

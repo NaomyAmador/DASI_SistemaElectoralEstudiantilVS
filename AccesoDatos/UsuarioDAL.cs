d5a4c0d249022d7cf93cecbc2731250a13504690
Arianna <ariannacedenob@gmail.com>
2026-05-04 17:56:31 -0400

    Ahora si ya estan hechos los acceso a datos

---
+++ AccesoDatos/UsuarioDAL.cs
@@ -0,0 +1,45 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class UsuarioDAL
+    {
+        ConexionBDD conexionBDD = new ConexionBDD();
+
+            public Usuarios ObtenerUsuarioPorID(int UsuarioID)
+            {
+                using (SqlConnection Conexion = conexionBDD.ObtenerConexion())
+                {
+                    SqlCommand usuario = new SqlCommand(
+                        @"SELECT UsuarioID, NombreCompleto, Usuario, Matricula, 
+                             Curso, Seccion, YaVoto, Activo, RolID, PadronID 
+                      FROM Usuarios WHERE UsuarioID = @UsuarioID", Conexion);
+
+                    usuario.Parameters.AddWithValue("@UsuarioID", UsuarioID);
+                    SqlDataReader reader = usuario.ExecuteReader();
+
+                    if (reader.Read())
+                        return new Usuarios
+                        {
+                            UsuarioID =         Convert.ToInt32(reader["UsuarioID"]),
+                            NombreCompleto =    reader["NombreCompleto"].ToString(),
+                            Matricula =         reader["Matricula"].ToString(),
+                            Curso =             reader["Curso"].ToString(),
+                            Seccion =           reader["Seccion"].ToString(),
+                            YaVoto =            Convert.ToBoolean(reader["YaVoto"]),
+                            Activo =            Convert.ToBoolean(reader["Activo"]),
+                            RolID =             Convert.ToInt32(reader["RolID"]),
+                            PadronID =          Convert.ToInt32(reader["PadronID"])
+                        };
+                    return null;
+                }
+            }
+        
+    }
+}

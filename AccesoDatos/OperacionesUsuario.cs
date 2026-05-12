fa8424b0a8bbac139be5e20c7e0971c20ac2a61d
Lia Karielys Torres Dominici <Dominicilia1@gmail.com>
2026-05-12 11:06:39 -0400

    Creacion de todo el acceso a datos del forms registro de usuarios

--- AccesoDatos/OperacionesUsuario.cs
+++ AccesoDatos/OperacionesUsuario.cs
@@ -12,7 +12,46 @@ namespace AccesoDatos
 {
     public class OperacionesUsuario
     {
+
+        Usuarios User;
         ConexionBDD conexionBDD = new ConexionBDD();
+
+        public Usuarios VerUsuarios(string Usuario, string @Contraseña)
+        {
+
+            using (SqlConnection conexion = conexionBDD.ObtenerConexion())
+            {
+                SqlCommand cmd = new SqlCommand("MostrarDatosUsuario", conexion);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Usuario", Usuario);
+                cmd.Parameters.AddWithValue("@Contraseña", Contraseña);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+
+                {
+                    Usuarios User = new Usuarios()
+
+                    {
+                        UsuarioID = Convert.ToInt32(dr["UsuarioID"]),
+                        NombreCompleto = dr["NombreCompleto"].ToString(),
+                        Usuario = dr["Usuario"].ToString(),
+                        Correo = dr["Correo"].ToString(),
+                        Curso = dr["Curso"].ToString(),
+                        Seccion = dr["Seccion"].ToString(),
+                        RolID = Convert.ToInt32(dr["RolID"])
+
+                    };
+
+                }
+                return User;
+
+
+            }
+
+        }
         public bool RegistrarUsusario(string NombreCompleto, string Usuario, String Contraseña, string Correo, string Matricula, string Curso, string Seccion)
         {
             using (SqlConnection conexion = conexionBDD.ObtenerConexion())
@@ -45,12 +84,9 @@ namespace AccesoDatos
 
         }
 
-
-
-
-        public bool EliminarUsuario(int ID)
-        {
-            try
+                public bool EliminarUsuario(int ID)
+                 {
+            try 
             {
                 using (SqlConnection conexion = conexionBDD.ObtenerConexion())
                 {
@@ -72,9 +108,40 @@ namespace AccesoDatos
                 return false;
             }
 
+        }
 
 
+
+        public bool ActualizarUsuario(Usuarios usuario)
+        {
+            try
+            {
+                using (SqlConnection conexion = conexionBDD.ObtenerConexion())
+                {
+                    SqlCommand command = new SqlCommand("ActualizarUsuario", conexion);
+
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("@NombreCompleto", usuario.NombreCompleto);
+                    command.Parameters.AddWithValue("@Correo", usuario.Correo);
+                    command.Parameters.AddWithValue("@Matricula", usuario.Matricula);
+                    command.Parameters.AddWithValue("@Curso", usuario.Curso);
+                    command.Parameters.AddWithValue("@Seccion", usuario.Seccion);
+
+                    command.ExecuteNonQuery();
+
+                    return true;
+
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+         
         }
-        }
+
     }
 }
+  

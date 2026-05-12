57b5f88a2505626bc7d55d9f694f9c577db6d2fb
Lia Karielys Torres Dominici <Dominicilia1@gmail.com>
2026-05-11 23:28:24 -0400

    Entidad creada, y primer Acceso a Datos

---
+++ AccesoDatos/RegistrarUsuario.cs
@@ -0,0 +1,52 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class RegistrarUsuario
+    {
+        ConexionBDD conexionBDD = new ConexionBDD();
+        public bool RegistrarUsusario(string NombreCompleto, string Usuario, String Contraseña, string Correo,string Matricula, string Curso, string Seccion)
+        {
+            using (SqlConnection conexion = conexionBDD.ObtenerConexion())
+            {
+
+                SqlCommand command = new SqlCommand("RegistrarUsuario", conexion);
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@NombreCompleto", NombreCompleto);
+                command.Parameters.AddWithValue("@Usuario", Usuario);
+                command.Parameters.AddWithValue("@Contraseña", Contraseña);
+                command.Parameters.AddWithValue("@Correo", Correo);
+                command.Parameters.AddWithValue("@Matricula", Matricula);
+                command.Parameters.AddWithValue("@Curso", Curso);
+                command.Parameters.AddWithValue("@Seccion", Seccion);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if ((reader.Read()))
+                {
+                    return true;
+
+                }
+
+                else
+                {
+                    return false;
+                }
+               
+            }
+
+           
+        }
+
+
+    }
+}
---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuarios
+    {
+        public int UsuarioID {  get; set; }
+        public string NombreCompleto { get; set; }
+        public string PasswordHash { get; set; }
+        public string Correo {  get; set; }
+        public String Matricula { get; set; }
+        public string Curso { get; set; }
+        public string Seccion { get; set; }
+        public bool YaVoto { get; set; }
+        public bool Activo { get; set; }
+        public int RolID{ get; set; }
+        public int PadronID { get; set; }
+
+
+
+
+    }
+}

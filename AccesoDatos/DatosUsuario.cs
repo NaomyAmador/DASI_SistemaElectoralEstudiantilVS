3ba67650e31903977feccae56aa3ef72effaf02a
naomyamador_04 <naomyabigail2009@gmail.com>
2026-05-14 23:16:43 -0400

    Clase UsuarioNegocio en LogicaNegocio creada

--- AccesoDatos/DatosUsuario.cs
+++ AccesoDatos/DatosUsuario.cs
@@ -46,6 +46,5 @@ namespace AccesoDatos
             return User;
         }
 
-        }
     }
 }
---
+++ LogicaNegocio/UsuarioNegocio.cs
@@ -0,0 +1,35 @@
+﻿using AccesoDatos;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio
+{
+    public class UsuarioNegocio
+    {
+        DatosUsuario Datos = new DatosUsuario();
+        public LoginUsuario Login(string usuario, string password)
+        {
+            if (usuario == "")
+            {
+                throw new Exception("Ingrese el Usuario");
+            }
+
+            if (password == "")
+            {
+                throw new Exception("Ingrese la Contraseña");
+            }
+
+            LoginUsuario User = Datos.Login(usuario, password);
+            if (User == null)
+            {
+                throw new Exception("Usuario o Contraseña incorrectas");
+            }
+
+            return User;
+        }
+    }
+}

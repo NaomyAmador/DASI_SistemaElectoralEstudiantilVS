87dd759e65993cbb63b8d280c857070dac9f66c8
naomyamador_04 <naomyabigail2009@gmail.com>
2026-05-14 23:00:43 -0400

    DatosUsuario en AccesoDatos con Procedimiento Sql creado

---
+++ AccesoDatos/DatosUsuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Entidades;
+
+namespace AccesoDatos
+{
+    public class DatosUsuario
+    {
+        ConexionBDD conexionBD = new ConexionBDD();
+        public LoginUsuario Login(string usuario, string password)
+        {
+            SqlConnection conexion = conexionBD.ObtenerConexion();
+            SqlCommand Procedimiento = new SqlCommand("LoginUsuario", conexion);
+            
+            Procedimiento.CommandType = CommandType.StoredProcedure;
+            Procedimiento.Parameters.AddWithValue("@Usuario", usuario);
+            Procedimiento.Parameters.AddWithValue("@Password", password);
+            
+            SqlDataReader LecturaDatos = Procedimiento.ExecuteReader();
+            //Variable vacía a utilizar más adelante
+            LoginUsuario User = null;
+
+        }
+    }
+}

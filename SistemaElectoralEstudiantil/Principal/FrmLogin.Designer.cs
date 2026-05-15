794923be89cfbe9c93da447951e368382291b761
naomyamador_04 <naomyabigail2009@gmail.com>
2026-05-15 10:35:19 -0400

    Nombre de objetos en FrmLogin establecidos

--- SistemaElectoralEstudiantil/Principal/FrmLogin.Designer.cs
+++ SistemaElectoralEstudiantil/Principal/FrmLogin.Designer.cs
@@ -31,22 +31,22 @@
             this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.TxtBox_Usuario = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
+            this.Lbl_Decoración1 = new System.Windows.Forms.Label();
+            this.Lbl_Decoración2 = new System.Windows.Forms.Label();
+            this.Lbl_Decoración3 = new System.Windows.Forms.Label();
+            this.Lbl_Decoración4 = new System.Windows.Forms.Label();
+            this.Lbl_Decoración5 = new System.Windows.Forms.Label();
+            this.Lbl_Indicaciones = new System.Windows.Forms.Label();
+            this.Lbl_Decoración7 = new System.Windows.Forms.Label();
+            this.Lbl_Decoración8 = new System.Windows.Forms.Label();
+            this.Lbl_Usuario = new System.Windows.Forms.Label();
+            this.Lbl_Contraseña = new System.Windows.Forms.Label();
             this.TxtBox_Password = new System.Windows.Forms.TextBox();
             this.Btn_IniciarSesión = new System.Windows.Forms.Button();
             this.Btn_VerPassword = new System.Windows.Forms.Button();
             this.Btn_NoVerPassword = new System.Windows.Forms.Button();
             this.ProgressBar_InicioSesión = new System.Windows.Forms.ProgressBar();
-            this.label11 = new System.Windows.Forms.Label();
+            this.Lbl_ProgressBarTexto = new System.Windows.Forms.Label();
             this.Tiempo_InicioSesión = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -56,111 +56,112 @@
             this.pictureBox1.Location = new System.Drawing.Point(13, 13);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(150, 141);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // TxtBox_Usuario
             // 
-            this.TxtBox_Usuario.Location = new System.Drawing.Point(73, 204);
+            this.TxtBox_Usuario.Location = new System.Drawing.Point(83, 204);
             this.TxtBox_Usuario.Name = "TxtBox_Usuario";
             this.TxtBox_Usuario.Size = new System.Drawing.Size(153, 22);
             this.TxtBox_Usuario.TabIndex = 1;
             // 
-            // label1
+            // Lbl_Decoración1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(170, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.Lbl_Decoración1.AutoSize = true;
+            this.Lbl_Decoración1.Location = new System.Drawing.Point(170, 27);
+            this.Lbl_Decoración1.Name = "Lbl_Decoración1";
+            this.Lbl_Decoración1.Size = new System.Drawing.Size(74, 16);
+            this.Lbl_Decoración1.TabIndex = 2;
+            this.Lbl_Decoración1.Text = "Título No. 1";
             // 
-            // label2
+            // Lbl_Decoración2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(170, 50);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.Lbl_Decoración2.AutoSize = true;
+            this.Lbl_Decoración2.Location = new System.Drawing.Point(170, 50);
+            this.Lbl_Decoración2.Name = "Lbl_Decoración2";
+            this.Lbl_Decoración2.Size = new System.Drawing.Size(74, 16);
+            this.Lbl_Decoración2.TabIndex = 3;
+            this.Lbl_Decoración2.Text = "Título No. 2";
             // 
-            // label3
+            // Lbl_Decoración3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(170, 74);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "label3";
+            this.Lbl_Decoración3.AutoSize = true;
+            this.Lbl_Decoración3.Location = new System.Drawing.Point(170, 74);
+            this.Lbl_Decoración3.Name = "Lbl_Decoración3";
+            this.Lbl_Decoración3.Size = new System.Drawing.Size(74, 16);
+            this.Lbl_Decoración3.TabIndex = 4;
+            this.Lbl_Decoración3.Text = "Título No. 3";
             // 
-            // label4
+            // Lbl_Decoración4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(171, 101);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(44, 16);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "label4";
+            this.Lbl_Decoración4.AutoSize = true;
+            this.Lbl_Decoración4.Location = new System.Drawing.Point(171, 101);
+            this.Lbl_Decoración4.Name = "Lbl_Decoración4";
+            this.Lbl_Decoración4.Size = new System.Drawing.Size(74, 16);
+            this.Lbl_Decoración4.TabIndex = 5;
+            this.Lbl_Decoración4.Text = "Título No. 4";
             // 
-            // label5
+            // Lbl_Decoración5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(171, 123);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(44, 16);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "label5";
+            this.Lbl_Decoración5.AutoSize = true;
+            this.Lbl_Decoración5.Location = new System.Drawing.Point(171, 123);
+            this.Lbl_Decoración5.Name = "Lbl_Decoración5";
+            this.Lbl_Decoración5.Size = new System.Drawing.Size(74, 16);
+            this.Lbl_Decoración5.TabIndex = 6;
+            this.Lbl_Decoración5.Text = "Título No. 5";
             // 
-            // label6
+            // Lbl_Indicaciones
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 167);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(44, 16);
-            this.label6.TabIndex = 7;
-            this.label6.Text = "label6";
+            this.Lbl_Indicaciones.AutoSize = true;
+            this.Lbl_Indicaciones.Location = new System.Drawing.Point(12, 167);
+            this.Lbl_Indicaciones.Name = "Lbl_Indicaciones";
+            this.Lbl_Indicaciones.Size = new System.Drawing.Size(83, 16);
+            this.Lbl_Indicaciones.TabIndex = 7;
+            this.Lbl_Indicaciones.Text = "Indicaciones";
             // 
-            // label7
+            // Lbl_Decoración7
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(599, 187);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(44, 16);
-            this.label7.TabIndex = 8;
-            this.label7.Text = "label7";
+            this.Lbl_Decoración7.AutoSize = true;
+            this.Lbl_Decoración7.Location = new System.Drawing.Point(487, 187);
+            this.Lbl_Decoración7.Name = "Lbl_Decoración7";
+            this.Lbl_Decoración7.Size = new System.Drawing.Size(170, 16);
+            this.Lbl_Decoración7.TabIndex = 8;
+            this.Lbl_Decoración7.Text = "Desarrollo de Aplicaciones";
             // 
-            // label8
+            // Lbl_Decoración8
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(599, 211);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(44, 16);
-            this.label8.TabIndex = 9;
-            this.label8.Text = "label8";
+            this.Lbl_Decoración8.AutoSize = true;
+            this.Lbl_Decoración8.Location = new System.Drawing.Point(509, 208);
+            this.Lbl_Decoración8.Name = "Lbl_Decoración8";
+            this.Lbl_Decoración8.Size = new System.Drawing.Size(148, 16);
+            this.Lbl_Decoración8.TabIndex = 9;
+            this.Lbl_Decoración8.Text = "y Sistemas Informáticos";
             // 
-            // label9
+            // Lbl_Usuario
             // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(14, 206);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(53, 20);
-            this.label9.TabIndex = 10;
-            this.label9.Text = "label9";
+            this.Lbl_Usuario.AutoSize = true;
+            this.Lbl_Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Usuario.Location = new System.Drawing.Point(14, 206);
+            this.Lbl_Usuario.Name = "Lbl_Usuario";
+            this.Lbl_Usuario.Size = new System.Drawing.Size(67, 20);
+            this.Lbl_Usuario.TabIndex = 10;
+            this.Lbl_Usuario.Text = "Usuario";
             // 
-            // label10
+            // Lbl_Contraseña
             // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(14, 253);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(62, 20);
-            this.label10.TabIndex = 11;
-            this.label10.Text = "label10";
+            this.Lbl_Contraseña.AutoSize = true;
+            this.Lbl_Contraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Contraseña.Location = new System.Drawing.Point(14, 253);
+            this.Lbl_Contraseña.Name = "Lbl_Contraseña";
+            this.Lbl_Contraseña.Size = new System.Drawing.Size(95, 20);
+            this.Lbl_Contraseña.TabIndex = 11;
+            this.Lbl_Contraseña.Text = "Contraseña";
             // 
             // TxtBox_Password
             // 
-            this.TxtBox_Password.Location = new System.Drawing.Point(82, 253);
+            this.TxtBox_Password.Location = new System.Drawing.Point(117, 253);
             this.TxtBox_Password.Name = "TxtBox_Password";
             this.TxtBox_Password.Size = new System.Drawing.Size(153, 22);
             this.TxtBox_Password.TabIndex = 12;
@@ -171,26 +172,26 @@
             this.Btn_IniciarSesión.Name = "Btn_IniciarSesión";
             this.Btn_IniciarSesión.Size = new System.Drawing.Size(131, 38);
             this.Btn_IniciarSesión.TabIndex = 13;
-            this.Btn_IniciarSesión.Text = "button1";
+            this.Btn_IniciarSesión.Text = "Iniciar Sesión";
             this.Btn_IniciarSesión.UseVisualStyleBackColor = true;
             this.Btn_IniciarSesión.Click += new System.EventHandler(this.Btn_IniciarSesión_Click);
             // 
             // Btn_VerPassword
             // 
-            this.Btn_VerPassword.Location = new System.Drawing.Point(250, 253);
+            this.Btn_VerPassword.Location = new System.Drawing.Point(283, 252);
             this.Btn_VerPassword.Name = "Btn_VerPassword";
-            this.Btn_VerPassword.Size = new System.Drawing.Size(32, 28);
+            this.Btn_VerPassword.Size = new System.Drawing.Size(41, 31);
             this.Btn_VerPassword.TabIndex = 14;
-            this.Btn_VerPassword.Text = "button2";
+            this.Btn_VerPassword.Text = "Ver";
             this.Btn_VerPassword.UseVisualStyleBackColor = true;
             // 
             // Btn_NoVerPassword
             // 
-            this.Btn_NoVerPassword.Location = new System.Drawing.Point(291, 253);
+            this.Btn_NoVerPassword.Location = new System.Drawing.Point(329, 252);
             this.Btn_NoVerPassword.Name = "Btn_NoVerPassword";
-            this.Btn_NoVerPassword.Size = new System.Drawing.Size(32, 28);
+            this.Btn_NoVerPassword.Size = new System.Drawing.Size(68, 31);
             this.Btn_NoVerPassword.TabIndex = 15;
-            this.Btn_NoVerPassword.Text = "button3";
+            this.Btn_NoVerPassword.Text = "No Ver";
             this.Btn_NoVerPassword.UseVisualStyleBackColor = true;
             // 
             // ProgressBar_InicioSesión
@@ -200,14 +201,14 @@
             this.ProgressBar_InicioSesión.Size = new System.Drawing.Size(625, 30);
             this.ProgressBar_InicioSesión.TabIndex = 16;
             // 
-            // label11
+            // Lbl_ProgressBarTexto
             // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(15, 381);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(51, 16);
-            this.label11.TabIndex = 17;
-            this.label11.Text = "label11";
+            this.Lbl_ProgressBarTexto.AutoSize = true;
+            this.Lbl_ProgressBarTexto.Location = new System.Drawing.Point(15, 381);
+            this.Lbl_ProgressBarTexto.Name = "Lbl_ProgressBarTexto";
+            this.Lbl_ProgressBarTexto.Size = new System.Drawing.Size(131, 16);
+            this.Lbl_ProgressBarTexto.TabIndex = 17;
+            this.Lbl_ProgressBarTexto.Text = "Procesando el Login";
             // 
             // Tiempo_InicioSesión
             // 
@@ -218,22 +219,22 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(659, 460);
-            this.Controls.Add(this.label11);
+            this.Controls.Add(this.Lbl_ProgressBarTexto);
             this.Controls.Add(this.ProgressBar_InicioSesión);
             this.Controls.Add(this.Btn_NoVerPassword);
             this.Controls.Add(this.Btn_VerPassword);
             this.Controls.Add(this.Btn_IniciarSesión);
             this.Controls.Add(this.TxtBox_Password);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Lbl_Contraseña);
+            this.Controls.Add(this.Lbl_Usuario);
+            this.Controls.Add(this.Lbl_Decoración8);
+            this.Controls.Add(this.Lbl_Decoración7);
+            this.Controls.Add(this.Lbl_Indicaciones);
+            this.Controls.Add(this.Lbl_Decoración5);
+            this.Controls.Add(this.Lbl_Decoración4);
+            this.Controls.Add(this.Lbl_Decoración3);
+            this.Controls.Add(this.Lbl_Decoración2);
+            this.Controls.Add(this.Lbl_Decoración1);
             this.Controls.Add(this.TxtBox_Usuario);
             this.Controls.Add(this.pictureBox1);
             this.Name = "FrmLogin";
@@ -247,22 +248,22 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox TxtBox_Usuario;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label Lbl_Decoración1;
+        private System.Windows.Forms.Label Lbl_Decoración2;
+        private System.Windows.Forms.Label Lbl_Decoración3;
+        private System.Windows.Forms.Label Lbl_Decoración4;
+        private System.Windows.Forms.Label Lbl_Decoración5;
+        private System.Windows.Forms.Label Lbl_Indicaciones;
+        private System.Windows.Forms.Label Lbl_Decoración7;
+        private System.Windows.Forms.Label Lbl_Decoración8;
+        private System.Windows.Forms.Label Lbl_Usuario;
+        private System.Windows.Forms.Label Lbl_Contraseña;
         private System.Windows.Forms.TextBox TxtBox_Password;
         private System.Windows.Forms.Button Btn_IniciarSesión;
         private System.Windows.Forms.Button Btn_VerPassword;
         private System.Windows.Forms.Button Btn_NoVerPassword;
         private System.Windows.Forms.ProgressBar ProgressBar_InicioSesión;
-        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label Lbl_ProgressBarTexto;
         private System.Windows.Forms.Timer Tiempo_InicioSesión;
     }
 }

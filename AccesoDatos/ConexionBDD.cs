73543e978ee64662cd346940328a8320209191f5
naomyamador_04 <naomyabigail2009@gmail.com>
2026-05-14 22:36:49 -0400

    Clase ConexionBDD en AccesoDatos creada

--- AccesoDatos/ConexionBDD.cs
+++ AccesoDatos/ConexionBDD.cs
@@ -9,7 +9,7 @@ namespace AccesoDatos
 {
     public class ConexionBDD
     {
-        private string CadenaConexion = "server=LAPTOP-PHTCMGVS\\SQLEXPRESS; database=SistemaVotacionEscolar; integrated security=true;";
+        private string CadenaConexion = "server=LAPTOP-9G07MQQC\\SQLEXPRESS; database=SistemaVotacionEscolar; integrated security=true;";
         //Servidor Naomy Amador: LAPTOP-9G07MQQC\SQLEXPRESS
         //Servidor Arianna Cedeño: LAPTOP-PHTCMGVS\SQLEXPRESS
         //Servidor Lía Torres: LILY\SQLEXPRESS
